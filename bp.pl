a5250de793c8e82b9157fde7dfa8d489aa438372
Adam Prime <adam.prime@wattpad.com>
2020-12-05 11:24:25 -0500

    new boiler plate, designed to have the input file piped to stdin

--- bp.pl
+++ bp.pl
@@ -1,23 +1,11 @@
+#!/usr/bin/env perl
 use 5.18.4;
 use strict;
 
-my $input = "a
-b
-c
-d
-e";
-
-my @lines = split /\n/, $input;
-my $answer;
-
-foreach my $line (@lines) {
-
-    say $line;
-    say 'next';
-
+while (<>) {
+    chomp;
+    say;
 }
-
-say $answer;
 
 
 
