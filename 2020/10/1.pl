0e5434628ee28aa7685b00e1af0cf0808af980dd
Adam Prime <adam.prime@wattpad.com>
2020-12-10 19:30:32 -0500

    10 done
    - did part 1 in the morning, ran out of time for part 2. had an idea of how to do it and finished in the evening

---
+++ 2020/10/1.pl
@@ -0,0 +1,30 @@
+use 5.18.4;
+use strict;
+use Data::Dumper;
+use File::Slurp;
+
+my @lines = read_file('input');
+@lines = sort {$a <=> $b} @lines;
+
+my %diff;
+
+
+foreach my $i (0 .. scalar @lines -1) {
+    chomp $lines[$i];
+    my $num = $lines[$i];
+    if ($i > 0) {
+        $diff{$num - $lines[$i-1]}++;
+    }
+    else{
+        $diff{$num}++;
+    }
+
+}
+$diff{3}++;
+
+say "done";
+say Dumper \%diff;
+say $diff{1} * $diff{3};
+
+
+
---
+++ 2020/10/2.pl
@@ -0,0 +1,22 @@
+use 5.18.4;
+use strict;
+use Data::Dumper;
+use File::Slurp;
+
+my @lines = read_file('input');
+@lines = sort {$a <=> $b} @lines;
+@lines = map { chomp $_; $_ } sort {$a <=> $b} @lines;
+push @lines, $lines[-1] + 3;
+
+my %routes = (0 => 1);
+
+foreach my $i (@lines) {
+
+    $routes{$i} = $routes{$i - 1} + $routes{$i-2} + $routes{$i-3};
+
+    say "$i: $routes{$i}"
+}
+
+
+
+
