1821f32c3049f2fd125b9ab2fad5a7c40a1de9a3
Adam Prime <adam.prime@wattpad.com>
2021-12-05 23:12:30 -0500

    day 5

---
+++ 2021/5/1.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/env perl
+use 5.18.4;
+use strict;
+use Data::Dumper;
+use List::Util qw/min max/;
+
+my $map = [];
+
+
+sub print_map {
+    my $map = shift;
+    foreach my $row (@$map) {
+        foreach my $val (@$row) {
+            print $val ? $val : '.'
+        }
+        print "\n";
+    }
+}
+
+sub scan_map {
+    my $map = shift;
+    my $count;
+    foreach my $row (@$map) {
+        foreach my $val (@$row) {
+            if ($val && $val > 1){
+                $count++;
+            }
+        }
+    }
+    return $count;
+}
+
+while (<>) {
+    chomp;
+
+    my @bits = split ' ';
+    my @p1 = split ',', $bits[0];
+    my @p2 = split ',', $bits[2];
+
+    if ($p1[1] == $p2[1]) {
+        if (!$map->[$p1[1]]) {
+            $map->[$p1[1]] = [];
+        }
+        for (my $i = min $p1[0], $p2[0]; $i <= max $p1[0], $p2[0]; $i++) {
+            $map->[$p1[1]]->[$i]++
+        }
+    }
+    elsif ($p1[0] == $p2[0]) {
+        for (my $i = min $p1[1], $p2[1]; $i <= max $p1[1], $p2[1]; $i++) {
+            if (!$map->[$i]) {
+                $map->[$i] = [];
+            }
+            $map->[$i]->[$p1[0]]++
+        }
+    }
+}
+
+say scan_map($map);
+
